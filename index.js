ef0ecb77e70a2f866ccc7a91ada39b7c89d89c43
Kenta Saito <kenta.saito@sagittar.org>
2023-10-19 22:54:07 +0900

    2023-10-19 22:10:07

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const module = await import(`./Server.js`);
-const Server = module.Server;
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
 
-//import { Server } from "./Server.js";
+serve(async (req) => {
+  const uuid = "https://deno.land/std@0.192.0/uuid/mod.ts";
+  const { version } = await import(uuid);
+  const ver = version("109156be-c4fb-41ea-b1b4-efe1671c5836");
+  return new Response(ver);
+});
+
+/*
+import { Server } from "./Server.js";
 //import { Site } from "./models/Site.js";
 
 //await Site.start("./misc/kv.sqlite3");
@@ -10,3 +17,4 @@ const Server = module.Server;
 Deno.serve(async (request) => {
   return await Server.response(request);
 });
+*/
